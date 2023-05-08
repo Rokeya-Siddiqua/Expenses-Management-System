3c8daad4b9a047b89daffa7f1dbd3bf3bfb62352
Rokeya-Siddiqua <jintu591998@gmail.com>
2023-05-08 16:37:21 +0600

    Initial commit

---
+++ Expenses.DataAccess/Repositories/IExpensesRepository.cs
@@ -0,0 +1,20 @@
+﻿using Expenses.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expenses.DataAccess.Repositories
+{
+    public interface IExpensesRepository
+    {
+        IEnumerable<ExpenseModel> GetAllExpenses();
+        IEnumerable<ExpenseModel> Search(string searchString);
+        void Add(ExpenseModel expense);
+        int Update(ExpenseModel expense);
+        ExpenseModel GetExpenseById(int id);
+        void Delete(int Id);
+
+    }
+}
---
+++ Expenses.Web/Controllers/ExpensesController.cs
@@ -0,0 +1,30 @@
+﻿using Expenses.DataAccess.Repositories;
+using Expenses.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Expenses.Web.Controllers
+{
+    public class ExpensesController : Controller
+    {
+        private readonly IExpensesRepository _expensesRepository;
+
+        public ExpensesController(IExpensesRepository expensesRepository)
+        {
+            _expensesRepository = expensesRepository;
+        }
+
+        public IActionResult Index(string searching)
+        {
+            List<ExpenseModel> lists = new List<ExpenseModel>();
+
+            if (string.IsNullOrEmpty(searching)) {
+                lists = _expensesRepository.GetAllExpenses().ToList();
+            }
+            else
+            {
+                lists = _expensesRepository.Search(searching).ToList();
+            }
+            return View(lists);
+        }
+    }
+}
